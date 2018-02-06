4ac05e173a3029d7568bddab5089b864f7a40f5d
bruce.wildish <bruce.wildish@scm.ca>
2018-02-06 11:43:38 -0500

    update todos location to include UID

--- app/app.jsx
+++ app/app.jsx
@@ -11,6 +11,7 @@ import router from 'app/router/';
 firebase.auth().onAuthStateChanged((user) => {
   if(user) {
     store.dispatch(actions.login(user.uid));
+    store.dispatch(actions.startAddTodos());
     hashHistory.push('/todos');
   } else {
     store.dispatch(actions.logout());
@@ -29,8 +30,8 @@ firebase.auth().onAuthStateChanged((user) => {
 // var initialTodos = TodoAPI.getTodos();
 // store.dispatch(actions.addTodos(initialTodos));
 
-// new way pulls todo collection from firebaseDB
-store.dispatch(actions.startAddTodos());
+// new way pulls todo collection from firebaseDB MOVED ABOVE
+//store.dispatch(actions.startAddTodos());
 
 // Load foundation for styles
 $(document).foundation();
